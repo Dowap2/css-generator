14078911e0c4e52a9cf8bc1dafd68b556cb25541
dowap2 <dowapdowari@gmail.com>
2020-06-17 19:09:33 +0900

    first commit

---
+++ node-practice/main.js
@@ -0,0 +1,22 @@
+var http = require("http");
+
+
+http.createServer(function(request, response){
+    /* 
+        HTTP 헤더 전송
+        HTTP Status: 200 : OK
+        Content Type: text/plain
+    */
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    /*
+        Response Body 를 "Hello World" 로 설정
+    */
+   var fs = require("fs")
+    var data = fs.readFileSync("ex.txt")
+    
+    
+    response.end(data.toString());
+}).listen(8080);
+
+console.log("Server running at http://127.0.0.1:8080");
