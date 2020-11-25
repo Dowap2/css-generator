792b9b1596930f92a891ac1af51f2932e585254e
dowap2 <dowapdowari@gmail.com>
2020-11-25 21:35:32 +0900

    Change css to styled-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
 import "../App.css";
+
 import { BoxWidthContainer } from "containers/BoxWidthContainer";
 import { BoxHeightContainer } from "containers/BoxHeightContainer";
 import { BoxBorderWidthContainer } from "containers/BoxBorderWidthContainer";
@@ -14,72 +17,79 @@ import { BoxTextModalContainer } from "containers/BoxTextModalContainer";
 import { ModalContainer } from "containers/ModalContainer";
 import { BoxColorTypeContainer } from "containers/BoxColorTypeContainer";
 import { AddAnimationContainer } from "containers/addAnimationContainer";
-import { useSelector } from "react-redux";
+
 import { PreviewBox } from "containers/PreviewBox";
 
+const Sidebar = styled.div`
+  background: #fff;
+  height: 100vh;
+  width: 20vw;
+  display: block;
+  float: left;
+`;
+const List = styled.li`
+  list-style: none;
+  margin: 0;
+  width: 80%;
+  padding: 20px;
+  margin: auto;
+  margin-top: 20px;
+  color: #594f3c;
+  background: #d9c8b4;
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.12);
+`;
+const DrawingPaper = styled.div`
+  background: #f5f5f5;
+  margin: auto;
+  height: 100vh;
+  width: 60vw;
+  display: inline-block;
+  float: left;
+`;
 function App() {
   const colorType = useSelector(state => state.boxState.colorType) || "color";
 
   return (
-    <div className="App">
+    <div>
       <ModalContainer />
-      <div className="Side-bar-left">
-        <menu className="menu-left">
-          <li className="menu-left-first">
-            length
-            <ul className="menu-left-first-submenu">
-              <BoxWidthContainer />
-              <BoxHeightContainer />
-            </ul>
-          </li>
-
-          <li className="menu-left-second">
-            border
-            <ul className="menu-left-second-submenu">
-              <BoxBorderWidthContainer />
-              <BoxBorderStyleContainer />
-              <BoxBorderColorContainer />
-            </ul>
-          </li>
-          <li className="menu-left-third">
-            color
-            <ul className="menu-left-third-submenu">
-              <BoxColorTypeContainer />
-              {colorType === "color" && <BoxColorContainer />}
-              {colorType === "gradient" && <BoxGradientComponent />}
-            </ul>
-          </li>
-        </menu>
-      </div>
-
-      <div className="Drawing-paper">
+      <Sidebar>
+        <List>
+          length
+          <BoxWidthContainer />
+          <BoxHeightContainer />
+        </List>
+        <List>
+          border
+          <BoxBorderWidthContainer />
+          <BoxBorderStyleContainer />
+          <BoxBorderColorContainer />
+        </List>
+        <List>
+          color
+          <BoxColorTypeContainer />
+          {colorType === "color" && <BoxColorContainer />}
+          {colorType === "gradient" && <BoxGradientComponent />}
+        </List>
+      </Sidebar>
+      <DrawingPaper>
         <AddAnimationContainer />
         <PreviewBox />
-      </div>
-
-      <div className="Side-bar-right">
-        <menu className="menu-right">
-          <li className="menu-right-first">
-            transform
-            <ul className="menu-right-first-submenu">
-              <BoxTransformContainer />
-            </ul>
-          </li>
-          <li className="menu-right-second">
-            rotate
-            <ul className="menu-right-second-submenu">
-              <BoxRotateContainer />
-            </ul>
-          </li>
-          <li className="menu-right-third">
-            text
-            <ul className="menu-right-third-submenu">
-              <BoxTextModalContainer />
-              <BoxTextContainer />
-            </ul>
-          </li>
-        </menu>
-      </div>
+      </DrawingPaper>
+      <Sidebar>
+        <List>
+          transform
+          <BoxTransformContainer />
+        </List>
+        <List>
+          rotate
+          <BoxRotateContainer />
+        </List>
+        <List>
+          text
+          <BoxTextModalContainer />
+          <BoxTextContainer />
+        </List>
+      </Sidebar>
     </div>
   );
 }
--- src/containers/PreviewBox.jsx
+++ src/containers/PreviewBox.jsx
@@ -1,40 +1,56 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+const DrawingPaper = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: ${props => props.width || "500px"};
+  height: ${props => props.height || "500px"};
+  border-width: ${props => props.borderWidth || "5px"};
+  border-color: ${props => props.borderColor || "#000000"};
+  border-style: ${props => props.borderStyle || "solid"};
+  transform: ${props =>
+    props.transform ||
+    "scaleX(1) scaleY(1)rotateX(0deg)rotateY(0deg)rotateZ(0deg)"};
+  text-align: ${props => props.textAlign || "left"};
+  color: ${props => props.color || "#000000"};
+  font-size: ${props => props.fontSize || "12px"};
+  background: ${props => props.background || "#ffffff"};
+`;
 
 export function PreviewBox(props) {
   const boxState = useSelector(state => state.boxState);
   const background =
     boxState.colorType === "gradient"
-      ? {
-          backgroundImage: `linear-gradient(${boxState.gradientStyle ||
-            "to top"},${boxState.color1 || "#000000"},${boxState.color2 ||
-            "#ffffff"})`
-        }
-      : {
-          backgroundColor: boxState.boxColor
-        };
+      ? `linear-gradient(${boxState.gradientStyle ||
+          "to top"},${boxState.color1 || "#000000"},${boxState.color2 ||
+          "#ffffff"})`
+      : boxState.boxColor || "#ffffff";
   const transform = `scaleX(${boxState.transformX ||
     1}) scaleY(${boxState.transformY || 1}) rotateX(${boxState.rotateX ||
     0}deg)rotateY(${boxState.rotateY || 0}deg)rotateZ(${boxState.rotateZ ||
     0}deg)`;
 
   return (
-    <div
-      className="box"
-      style={{
-        width: boxState.boxWidth,
-        height: boxState.boxHeight,
-        borderWidth: boxState.borderWidth,
-        borderStyle: boxState.borderStyle,
-        borderColor: boxState.borderColor,
-        transform: transform,
-        textAlign: boxState.textStyle,
-        color: boxState.textColor,
-        fontSize: boxState.fontSize + "px",
-        ...background
-      }}
+    <DrawingPaper
+      width={boxState.boxWidth}
+      height={boxState.boxHeight}
+      borderWidth={boxState.borderWidth}
+      borderStyle={boxState.borderStyle}
+      borderColor={boxState.borderColor}
+      transform={transform}
+      textAlign={boxState.textStyle}
+      color={boxState.textColor}
+      fontSize={boxState.fontSize}
+      background={background}
     >
       {boxState.text}
-    </div>
+    </DrawingPaper>
   );
 }
