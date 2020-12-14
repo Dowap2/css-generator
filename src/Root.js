fcf73eaadf5535b494e6369f309f1d502021a939
dowap2 <dowapdowari@gmail.com>
2020-12-14 17:59:00 +0900

    modify Route

--- src/Root.js
+++ src/Root.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "store";
 
-import App from "./components/App";
+import App from "components/App";
 
 const Root = () => {
   return (
