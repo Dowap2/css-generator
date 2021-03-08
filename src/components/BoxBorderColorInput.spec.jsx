fc18e19b97d7adadf977b8e7c3c4c041444a7361
dowap2 <dowapdowari@gmail.com>
2021-03-08 20:28:43 +0900

    add jest

---
+++ src/components/BoxBorderColorInput.spec.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Store } from "redux";
+import { BoxBorderColorInput } from "./BoxBorderColorInput";
+
+test("1 is 1", () => {
+  expect(1).toBe(1);
+});
+
+describe("BoxBorderColor", () => {
+  const state = {
+    color: "#000000"
+  };
+  it("BorderColor Change", () => {});
+});
