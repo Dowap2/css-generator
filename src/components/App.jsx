b36834dbbb16a34700587ac83a11e77ce360f99d
dowap2 <dowapdowari@gmail.com>
2021-03-26 22:07:49 +0900

    color picker change color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { Route, BrowserRouter } from "react-router-dom";
 import { LoginContainer } from "containers/LoginContainer";
@@ -9,9 +9,10 @@ function App() {
   // if (window.location.href === "http://localhost:3000/") {
   //   window.location.href = "http://localhost:3000/main";
   // }
+  const [color, setColor] = useState("#ffffff");
   return (
     <BrowserRouter basename="/css-generator">
-      <SketchPicker />
+      <SketchPicker color={color} onChangeComplete={setColor} />
       <Route exact path="/login" component={LoginContainer} />
       <Route path="" component={MainComponent} />
     </BrowserRouter>
