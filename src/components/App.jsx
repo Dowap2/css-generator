1f0b115bffb760af5fc322cad658a672b39272dd
dowap2 <dowapdowari@gmail.com>
2021-03-26 21:56:58 +0900

    add color picker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import "../App.css";
 import { Route, BrowserRouter } from "react-router-dom";
 import { LoginContainer } from "containers/LoginContainer";
 import { MainComponent } from "components/MainComponent";
+import { SketchPicker } from "react-color";
 
 function App() {
   // if (window.location.href === "http://localhost:3000/") {
@@ -10,6 +11,7 @@ function App() {
   // }
   return (
     <BrowserRouter basename="/css-generator">
+      <SketchPicker />
       <Route exact path="/login" component={LoginContainer} />
       <Route path="" component={MainComponent} />
     </BrowserRouter>
