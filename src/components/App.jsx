a5ceca3ad2e6ffc779f58491cc391d23ba1a5366
dowap2 <dowapdowari@gmail.com>
2021-03-29 21:10:18 +0900

    remove color picker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "../App.css";
 import { Route, BrowserRouter } from "react-router-dom";
 import { LoginContainer } from "containers/LoginContainer";
 import { MainComponent } from "components/MainComponent";
-import { SketchPicker } from "react-color";
 
 function App() {
   // if (window.location.href === "http://localhost:3000/") {
   //   window.location.href = "http://localhost:3000/main";
   // }
-  const [color, setColor] = useState("#ffffff");
   return (
     <BrowserRouter>
-      <SketchPicker color={color} onChangeComplete={setColor} />
       <Route exact path="/login" component={LoginContainer} />
       <Route path="" component={MainComponent} />
     </BrowserRouter>
