35031dd86b27aa9435a2ecb417aeca07d6e4cc6a
dowap2 <dowapdowari@gmail.com>
2021-06-14 21:55:46 +0900

    add format date

--- src/components/ChangeLanguageComponent.jsx
+++ src/components/ChangeLanguageComponent.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export function ChangeLanguageComponent(props) {
   return (
     <select name="lan" id="lan" onChange={e => props.onChange(e.target.value)}>
-      <option value="ko">Korean</option>
       <option value="en">English</option>
+      <option value="ko">Korean</option>
     </select>
   );
 }
--- src/components/ClockComponent.jsx
+++ src/components/ClockComponent.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import moment from "moment";
 import ChangeLanguageContainer from "containers/ChangeLanguageContainer";
 
 export function ClockComponent(props) {
-  const date = moment().format("LLLL");
+  const language = useSelector(state => state.languageState.state.language);
+  const [format, setFormat] = useState(moment().locale("en"));
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    language === "en"
+      ? setFormat(moment().locale("en"))
+      : setFormat(moment().locale("ko"));
+  }, [language]);
+
+  setInterval(() => {
+    setDate(format.format("LTS"));
+  }, 1000);
+
   return (
     <div>
       {date}
